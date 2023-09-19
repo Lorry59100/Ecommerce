5f3d53d1db438c8ea3477c51becffd5f1bb2e516
pisha <pisha@hotmail.fr>
2023-09-19 14:52:31 +0200

    Axios import and intialvalues adjustments

--- frontend/src/components/forms/RegistrationForm.js
+++ frontend/src/components/forms/RegistrationForm.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Formik, Form } from 'formik'
 import * as Yup from 'yup'
 import FormikControl from '../FormikControl'
+import axios from 'axios';
 
 export function RegistrationForm() {
 
@@ -28,9 +29,27 @@ export function RegistrationForm() {
         confirmPassword: Yup.string().oneOf([Yup.ref('password'), ''], 'Les mots de passe ne correspondent pas').required('Requis'),
     })
 
-    const onSubmit = values => {
-        console.log('Form data', values)
-    }
+    const onSubmit = (values) => {
+        console.log('Values', values);
+        axios
+          .post('https://127.0.0.1:8000/api/register', {
+            firstname: values.firstname,
+            lastname: values.lastname,
+            address: values.address,
+            cp: values.cp,
+            town: values.town,
+            country: values.country,
+            email: values.email,
+            password: values.password
+          }) // Send values along with the request
+          .then((response) => {
+            console.log('Response data', response.data);
+            // Handle the response data as needed
+          })
+          .catch((error) => {
+            console.error('Erreur lors de la récupération des données :', error);
+          });
+      };
 
     return <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={onSubmit}> 
         {
