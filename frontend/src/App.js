356ccc7d10acb1f2584cd038386445f670812434
pisha <pisha@hotmail.fr>
2023-09-20 16:17:37 +0200

    FormikContainer, LoginForm, RegistrationForm imports

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import FormikContainer from './components/FormikContainer';
 import ProductList from './components/ProductList';
+import LoginForm from './components/forms/LoginForm';
+import { RegistrationForm } from './components/forms/RegistrationForm';
 
 function App() {
   return (
     <div className="App">
-      <ProductList/>
+      <LoginForm/>
     </div>
   );
 }
