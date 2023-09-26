0960d79e32a7a4365f3f8fff96fa873e878979f3
pisha <pisha@hotmail.fr>
2023-09-26 14:21:34 +0200

    toastify success msg and userId fixed

--- frontend/src/components/ProductList.js
+++ frontend/src/components/ProductList.js
@@ -1,10 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
+import jwt_decode from 'jwt-decode';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function ProductList() {
   const [products, setProducts] = useState([]);
-
+  const token = localStorage.getItem('authToken');
+  const decodedToken = jwt_decode(token);
+  const userId = decodedToken.id;
+  const notify = () => toast.success("Produit ajouté au panier");
   useEffect(() => {
     axios.get('https://127.0.0.1:8000/api/products')
       .then(response => {
@@ -51,7 +57,6 @@ function ProductList() {
 
   const addToCart = (product) => {
     // Envoie une requête pour ajouter le produit au panier
-    const userId = 1; // Remplacez par l'ID de l'utilisateur actuel
     const quantity = product.quantity;
 
     axios.post(`https://127.0.0.1:8000/api/addcart/${userId}/${product.id}/${quantity}`)
@@ -67,25 +72,29 @@ function ProductList() {
 
   return (
     <div>
+      <ToastContainer />
       <h1>Liste des produits</h1>
       <div className="product-cards">
         {products.map(product => (
           <div key={product.id} className="product-card">
             <div className="title_card">
               <h2>{product.name}</h2>
-            <Link to={`/single_product/${product.id}`}><button>Détails</button></Link>
+            
             </div>
               <img src={product.img} alt={product.name} />
-              <p>Prix : {product.price} €</p>
+              <div className="stock-price">
+                <p> Stock : {product.stock} </p>
+                <p>Prix : {product.price * product.quantity} €</p>
+              </div>
               <div className="card-buttons">
                 <div className="increment-buttons">
               <button onClick={() => decrementQuantity(product)}>-</button>
               <span>{product.quantity}</span> 
               <button onClick={() => incrementQuantity(product)}>+</button>
                 </div>
-              <button onClick={() => addToCart(product)}>Ajouter au panier</button>
+                <button onClick={() => { addToCart(product); notify(); }}>Ajouter au panier</button>
               </div>
-            
+              <Link to={`/single_product/${product.id}`}><button className='detail-btn'>Détails</button></Link>
           </div>
         ))}
       </div>
