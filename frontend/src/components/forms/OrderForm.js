2383bfa9879ee6b06d5c3afaac78db8c040d3d70
pisha <pisha@hotmail.fr>
2023-09-26 11:26:36 +0200

    redesign, use of Formik & Yup, and redirection after submit

--- frontend/src/components/forms/OrderForm.js
+++ frontend/src/components/forms/OrderForm.js
@@ -3,52 +3,74 @@ import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import axios from 'axios';
 import jwt_decode from 'jwt-decode';
+import { Formik, Field, Form, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+import { Navigate } from 'react-router-dom';
 
 function OrderForm() {
-    const token = localStorage.getItem('authToken');
+  const token = localStorage.getItem('authToken');
   const decodedToken = jwt_decode(token);
-  const userId = decodedToken.id; // Déclarez userId dans la portée supérieure
+  const userId = decodedToken.id;
   const [selectedDate, setSelectedDate] = useState(null);
+  const [redirectTo, setRedirectTo] = useState(null);
+
+
+  const validationSchema = Yup.object().shape({
+    selectedDate: Yup.date()
+      .required('La date est requise')
+      .min(new Date(), 'La date ne peut pas être antérieure à aujourd\'hui'),
+  });
 
   const handleDateChange = (date) => {
     setSelectedDate(date);
   };
 
-  const handleSubmit = () => {
-    // Assurez-vous que selectedDate n'est pas nul avant d'envoyer la requête
-    if (selectedDate) {
-      // Préparez les données du formulaire à envoyer au serveur
-      const formData = {
-        selectedDate: selectedDate.toISOString(), // Convertissez la date en format ISO
-      };
-
-      console.log(formData.selectedDate);
+  const submitForm = (values, { setSubmitting }) => {
+    // Effectuez la requête HTTP POST vers votre API Symfony avec les valeurs du formulaire
+    axios
+      .post(`https://127.0.0.1:8000/api/confirm/${userId}/${values.selectedDate.toISOString()}`)
+      .then(response => {
+        // Traitement de la réponse du serveur en cas de succès
+        console.log('La requête a été soumise avec succès.');
+        setSubmitting(false); // Marquez le formulaire comme soumis avec succès
   
-      // Effectuez la requête HTTP POST vers votre API Symfony
-      axios.post(`https://127.0.0.1:8000/api/confirm/${userId}/${formData.selectedDate}`)
-        .then(response => {
-          // Traitement de la réponse du serveur en cas de succès
-          console.log('Réponse du serveur :', response.data);
-        })
-        .catch(error => {
-          // Gestion des erreurs en cas d'échec de la requête
-          console.error('Erreur lors de la requête POST :', error);
-        });
-    } else {
-      console.error('La date n\'est pas sélectionnée.');
-    }
+        // Définissez la valeur de redirectTo pour déclencher la redirection
+        setRedirectTo('/'); // Remplacez '/nouvelle-page' par l'URL de la page de destination
+      })
+      .catch(error => {
+        // Gestion des erreurs en cas d'échec de la requête
+        console.error('Erreur lors de la soumission du formulaire :', error);
+        setSubmitting(false); // Marquez le formulaire comme soumis avec échec
+      });
   };
-
+  
   return (
-    <div>
-      <h2>Date de livraison</h2>
-      <DatePicker
-        selected={selectedDate}
-        onChange={handleDateChange}
-        dateFormat="dd/MM/yyyy" // Format de date souhaité
-      />
-      <button onClick={handleSubmit}>Valider la livraison</button>
-    </div>
+    <Formik
+      initialValues={{ selectedDate: null }}
+      validationSchema={validationSchema}
+      onSubmit={submitForm}
+    >
+      {({ isSubmitting, setFieldValue }) => (
+        <Form className="form-order">
+          <h2>Date de livraison</h2>
+          <DatePicker
+            dateFormat="dd/MM/yyyy"
+            selected={selectedDate}
+            onChange={(date) => {
+              setFieldValue('selectedDate', date); // Met à jour la valeur du champ
+              setSelectedDate(date); // Met à jour l'état local
+            }}
+            minDate={new Date()}
+          />
+          <ErrorMessage name="selectedDate" component="div" className="error" />
+
+          <button type="submit" disabled={isSubmitting}>
+            Valider la livraison
+          </button>
+          {redirectTo && <Navigate to={redirectTo} />}
+        </Form>
+      )}
+    </Formik>
   );
 }
 
