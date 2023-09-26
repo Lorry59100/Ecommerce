05033c47a16eb1d7640e06b4aab60110970a0bcb
pisha <pisha@hotmail.fr>
2023-09-26 14:25:00 +0200

    supress of unused import

--- frontend/src/components/forms/RegistrationForm.js
+++ frontend/src/components/forms/RegistrationForm.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { Formik, Form } from 'formik'
-import * as Yup from 'yup'
 import FormikControl from '../FormikControl'
 import axios from 'axios';
 import { ValidationRegister } from './ValidationRegister';
