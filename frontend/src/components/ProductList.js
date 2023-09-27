b92dae56a49f9d0c7853d59421ae1893d8fd36b7
pisha <pisha@hotmail.fr>
2023-09-27 10:56:34 +0200

    Refactoring filter

--- frontend/src/components/ProductList.js
+++ frontend/src/components/ProductList.js
@@ -9,15 +9,16 @@ function ProductList() {
   const [products, setProducts] = useState([]);
   const [selectedCategory, setSelectedCategory] = useState('');
   const token = localStorage.getItem('authToken');
-  const decodedToken = token ? jwt_decode(token) : null; // Vérifiez si le token existe
-  const userId = decodedToken ? decodedToken.id : null; // Utilisez le userId uniquement si le token existe
+  const decodedToken = token ? jwt_decode(token) : null;
+  const userId = decodedToken ? decodedToken.id : null;
   const notify = () => toast.success("Produit ajouté au panier");
+
   useEffect(() => {
     axios.get('https://127.0.0.1:8000/api/products')
       .then(response => {
         const productsWithQuantity = response.data.map(product => ({
           ...product,
-          quantity: 1, // Initialisez la quantité à zéro
+          quantity: 1,
         }));
         setProducts(response.data);
         setProducts(productsWithQuantity);
@@ -27,83 +28,70 @@ function ProductList() {
       });
   }, []);
 
-  // Fonction pour filtrer les produits par catégorie
-  const filterProductsByCategory = () => {
-    if (selectedCategory === '') {
-      return products; // Retournez tous les produits si aucune catégorie n'est sélectionnée
-    } else {
-      return products.filter(product => product.category === selectedCategory);
-    }
+  const getUniqueCategories = () => {
+    const uniqueCategories = [...new Set(products.map(product => product.category))];
+    return uniqueCategories;
   };
 
   const incrementQuantity = (product) => {
-    // Créez une copie des produits avec la quantité mise à jour
     const updatedProducts = products.map(p => {
       if (p.id === product.id) {
         return { ...p, quantity: p.quantity + 1 };
       }
       return p;
     });
-  
-    // Mettez à jour l'état avec les produits mis à jour
     setProducts(updatedProducts);
   };
-  
+
   const decrementQuantity = (product) => {
-    // Vérifiez que la quantité ne devient pas négative
     if (product.quantity > 1) {
-      // Créez une copie des produits avec la quantité mise à jour
       const updatedProducts = products.map(p => {
         if (p.id === product.id) {
           return { ...p, quantity: p.quantity - 1 };
         }
         return p;
       });
-  
-      // Mettez à jour l'état avec les produits mis à jour
       setProducts(updatedProducts);
     }
   };
 
   const addToCart = (product) => {
-    // Envoie une requête pour ajouter le produit au panier
     const quantity = product.quantity;
 
     axios.post(`https://127.0.0.1:8000/api/addcart/${userId}/${product.id}/${quantity}`)
       .then(response => {
-        // Traitez la réponse si nécessaire (par exemple, affichez un message de succès)
         console.log('Produit ajouté au panier avec succès', response.data);
       })
       .catch(error => {
         console.error('Erreur lors de l\'ajout du produit au panier :', error);
       });
   };
-  
 
   return (
     <div>
       <ToastContainer />
       <h1>Liste des produits</h1>
 
-    {/* Sélecteur de catégorie */}
-    <select
+      {/* Sélecteur de catégorie avec les options générées à partir des catégories uniques */}
+      <select
         value={selectedCategory}
         onChange={(e) => setSelectedCategory(e.target.value)}
       >
         <option value="">Toutes les catégories</option>
-        <option value="Console">Consoles</option>
-        <option value="Jeux Vidéos">Jeux Vidéos</option>
-        <option value="Manettes">Manettes</option>
+        {getUniqueCategories().map((category, index) => (
+          <option key={index} value={category}>{category}</option>
+        ))}
       </select>
 
       <div className="product-cards">
-        {filterProductsByCategory().map(product => (
-          <div key={product.id} className="product-card">
-            <div className="title_card">
-              <h2>{product.name}</h2>
-              <h5>{product.category}</h5>
-            
-            </div>
+        {products
+          .filter(product => selectedCategory === '' || product.category === selectedCategory)
+          .map(product => (
+            <div key={product.id} className="product-card">
+              <div className="title_card">
+                <h2>{product.name}</h2>
+                <h5>{product.category}</h5>
+              </div>
               <img src={product.img} alt={product.name} />
               <div className="stock-price">
                 <p> Stock : {product.stock} </p>
@@ -111,15 +99,15 @@ function ProductList() {
               </div>
               <div className="card-buttons">
                 <div className="increment-buttons">
-              <button onClick={() => decrementQuantity(product)}>-</button>
-              <span>{product.quantity}</span> 
-              <button onClick={() => incrementQuantity(product)}>+</button>
+                  <button onClick={() => decrementQuantity(product)}>-</button>
+                  <span>{product.quantity}</span>
+                  <button onClick={() => incrementQuantity(product)}>+</button>
                 </div>
                 <button onClick={() => { addToCart(product); notify(); }}>Ajouter au panier</button>
               </div>
               <Link to={`/single_product/${product.id}`}><button className='detail-btn'>Détail</button></Link>
-          </div>
-        ))}
+            </div>
+          ))}
       </div>
     </div>
   );
