519aedc73705d419fa1b100a0817bd03bda141ab
pisha <pisha@hotmail.fr>
2023-10-03 10:59:56 +0200

    redesign of Stripe elements and field control before submit

--- frontend/src/components/forms/CheckoutForm.js
+++ frontend/src/components/forms/CheckoutForm.js
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
-import { useStripe, useElements, CardElement } from '@stripe/react-stripe-js';
+import React, { useState, useEffect } from 'react';
+import { useStripe, useElements, CardNumberElement, CardExpiryElement, CardCvcElement } from '@stripe/react-stripe-js';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import axios from 'axios';
 
-const CheckoutForm = ({ userId, totalPrice, onClientSecretReceived }) => {
+const CheckoutForm = ({ userId, totalPrice }) => {
   const [selectedDate, setSelectedDate] = useState(null);
   const [isFormValid, setIsFormValid] = useState(false);
 
@@ -25,6 +25,34 @@ const CheckoutForm = ({ userId, totalPrice, onClientSecretReceived }) => {
     }
   };
 
+    const handleBlur = (element, errorId, errorMessage) => {
+      element.on('blur', (event) => {
+        const displayError = document.getElementById(errorId);
+        if (element._parent.classList.contains('StripeElement--empty') || element._parent.classList.contains('StripeElement--invalid')) {
+          displayError.textContent = errorMessage;
+        } else {
+          displayError.textContent = ''; // Efface le message d'erreur si le champ est valide
+        }
+      });
+    };
+
+    useEffect(() => {
+      if (elements) {
+        const elementsToCheck = [
+          { element: elements.getElement(CardNumberElement), errorId: 'card-errors-number', errorMessage: 'Numéro de carte incomplet ou invalide.' },
+          { element: elements.getElement(CardExpiryElement), errorId: 'card-errors-expiry', errorMessage: 'Date d\'expiration incomplète ou invalide' },
+          { element: elements.getElement(CardCvcElement), errorId: 'card-errors-cvc', errorMessage: 'CVC incomplet ou invalide' }
+        ];
+    
+        elementsToCheck.forEach(({ element, errorId, errorMessage }) => {
+          if (element) {
+            handleBlur(element, errorId, errorMessage);
+          }
+        });
+      }
+    }, [elements]);
+    
+
   const handleSubmit = async (event) => {
     event.preventDefault();
 
@@ -33,11 +61,16 @@ const CheckoutForm = ({ userId, totalPrice, onClientSecretReceived }) => {
       return;
     }
 
-    const cardElement = elements.getElement(CardElement);
+    const cardnumberElement = elements.getElement(CardNumberElement);
+    console.log(cardnumberElement);
+    const cardexpiryElement = elements.getElement(CardExpiryElement);
+    const cardcvcElement = elements.getElement(CardCvcElement);
 
     const paymentData = {
       payment_method: {
-        card: cardElement,
+        card: cardnumberElement,
+        card: cardexpiryElement,
+        card: cardcvcElement,
       },
     };
 
@@ -69,7 +102,23 @@ const CheckoutForm = ({ userId, totalPrice, onClientSecretReceived }) => {
     <form onSubmit={handleSubmit}>
       <label>
         Détails de la carte
-        <CardElement className="CardElement" onChange={(e) => handleFormValidation(e.complete)} />
+        <div className="card-element-container">
+          <span className='card-info'>Numéro de carte</span>
+        <CardNumberElement className="CardNumberElement" onChange={(e) => handleFormValidation(e.complete)} />
+        <div id="card-errors-number" className='error'></div>
+        </div>
+
+        <div className="card-element-container">
+        <span className='card-info'>Date d'expiration</span>
+        <CardExpiryElement className="CardExpiryElement" onChange={(e) => handleFormValidation(e.complete)} />
+        <div id="card-errors-expiry" className='error'></div>
+        </div>
+
+        <div className="card-element-container">
+        <span className='card-info'>CVC</span>
+        <CardCvcElement className="CardCvcElement" onChange={(e) => handleFormValidation(e.complete)} />
+        <div id="card-errors-cvc" className='error'></div>
+        </div>
       </label>
 
       <div>
